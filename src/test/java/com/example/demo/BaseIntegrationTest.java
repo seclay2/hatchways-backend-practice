08d6814477a8cab7cf2ed3866ca27d821e019008
Scott Clay <28816963+seclay2@users.noreply.github.com>
2022-03-31 21:07:00 -0600

    add base test classes

---
+++ src/test/java/com/example/demo/BaseIntegrationTest.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Tag;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Tag("Integration")
+public class BaseIntegrationTest {
+}
